48bbdab07690d093d6f2bc7ecc59d57b2fa28e04
AravaSaiKrishna1911 <saikrishnaarava27@gmail.com>
2025-03-26 16:47:46 +0530

    multiple commit

---
+++ cd.java
@@ -0,0 +1,5 @@
+public class cd {
+    public static void main(String[] args) {
+        System.out.println("welcome to cd class");
+    }
+}
---
+++ labexam.java
@@ -0,0 +1,14 @@
+public class labexam {
+
+    
+
+public static void main(String[] args) {
+
+
+System.out.println("Welcome to cd lab exam");
+
+
+    
+}
+
+}
